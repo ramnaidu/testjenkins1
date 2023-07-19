11b71c5e4a2836472ef4464aaf89740ab2cf9d86
ramnaidu <s.ramnaidu7@gmail.com>
2023-07-19 16:09:50 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,9 @@
+import subprocess
 string = "hello world.. this is python file"
 print(string)
 ls = ["welcome","to","jenkins"]
 for i in ls:
   print(i+" ")
+print("pytest version is:")
+proc = subprocess.run('pytest --version',shell=True,stdout=subprocess.PIPE)
+print(proc.stdout)
