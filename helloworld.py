c5e0d302299543270c0eb38918207a6bf9a634f1
ramnaidu <s.ramnaidu7@gmail.com>
2023-07-19 16:14:17 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -5,5 +5,6 @@ ls = ["welcome","to","jenkins"]
 for i in ls:
   print(i+" ")
 print("pytest version is:")
+subprocess.run('pip install -r requirements.txt',shell=True)
 proc = subprocess.run('pytest --version',shell=True,stdout=subprocess.PIPE)
 print(proc.stdout.decode("utf-8"))
